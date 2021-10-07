364136a7b56ac1e3585a919d4c4478b1645b7929
Madari Sindhu <77852652+Madarisindhu@users.noreply.github.com>
2021-10-07 16:25:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Assests from './components/Assests';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Section from './components/Section';
+import Services from './components/Services';
+
+
+
+const App = () => {
+  return (
+    <>
+    <Home />
+    <Services />
+    <Assests />
+    <Section />
+    <Footer />
+   </>
+  )
+}
+
+export default App
+
+
+
+
